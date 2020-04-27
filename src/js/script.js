de43b8dd17ff6d284052a02da78391066e1e6f14
Tomas Valkendorff <Tomvalken1@gmail.com>
2020-04-27 11:08:40 +0300

    Revert "Added comments to edits"
    This reverts commit 8d9428a87e00f55c17a589749e328a997801e5ec.

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,17 @@
 function initMap() {
-    //Element containing coordinates
     var karaportti = { lat: 60.224289, lng: 24.758614 };
-    //Creates a map with zoom lvl 12 centered around karaportti
     var map = new google.maps.Map(
         document.getElementById('map'), { zoom: 12, center: karaportti });
-    //Creates a marker in karaportti with title karaportti 2
     var marker = new google.maps.Marker({
         position: karaportti,
         map: map,
         title: 'Karaportti 2'
       });
-    //Creates infowindow with h1 tag
+    
     var infoWindow = new google.maps.InfoWindow({
         content : '<h1>Karaportti 2, Metropolia</h1>'
     });
-    //Adds listener to marker and opens infowindow
+
     marker.addListener('click', function() {
         infoWindow.open(map, marker)
     });
