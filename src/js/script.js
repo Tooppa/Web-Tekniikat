f83aaadee4d7f08059422b8ca49fcd888151e4ac
leohu1999 <leo.huovinen@metropolia.fi>
2020-04-30 13:39:41 +0300

    Aloitus zoom

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 
-var luku = 1;
+var luku = 0;
 function sijainti(clicked) {
     luku = clicked;
     initMap();
@@ -39,25 +39,35 @@ function initMap() {
     //coordinates and images
     var markers = [
         {
+            zoom:10,
+            koordinaatit: { lat: 60.182215, lng: 24.913401 },
+
+        },
+        {
+            zoom:14,
             koordinaatit: { lat: 60.182215, lng: 24.913401 },
             image: '<img id="0" src="../src/images/sibelius-4738705_640.jpg" width="300" height="200" onclick="isonna(id)"></img>',
             info: '<p>Avaa isompana painamalla kuvaa</p>'
         },
         {
+            zoom:14,
             koordinaatit: { lat: 60.144491, lng: 24.989855 },
             image: '<img id="1" src="../src/images/suomenlinna-334371_640.jpg" width="300" height="200" onclick="isonna(id)"></img>',
             info: '<p>Avaa isompana painamalla kuvaa</p>'
         },
         {
+            zoom:14,
             koordinaatit: { lat: 60.170495, lng: 24.952144 },
             image: '<img id="2" src="../src/images/helsinki-4984737_640.jpg" width="300" height="200" onclick="isonna(id)"></img>',
             info: '<p>Avaa isompana painamalla kuvaa</p>'
         },
         {
+            zoom : 14,
             koordinaatit: { lat: 60.2933611820065, lng: 24.86623741574194 },
             image: '<img src="images/vantaankoski-1710134_640.jpg" width="300" height="200"></img>'
         },
         {
+            zoom: 14,
             koordinaatit: { lat: 60.174575, lng: 24.984599398411612 },
             image: '<img id="4" src="images/tiger-1415765_640.jpg" width="300" height="200" onclick="isonna(id)"></img>',
             info: '<p>Avaa isompana painamalla kuvaa</p>'
@@ -66,7 +76,7 @@ function initMap() {
 
     //Creates a map with zoom lvl 12 centered around first element of array
     var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 14, center: markers[luku].koordinaatit });
+        document.getElementById('map'), { zoom: markers[luku].zoom, center: markers[luku].koordinaatit });
     // creates markers from array
     function addMarker(props) {
         var marker = new google.maps.Marker({
